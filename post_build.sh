cc75f1eccbe9e6edee02f6cc5127d83114de9403
Krogoth <le-krogoth@users.noreply.github.com>
2024-07-28 23:18:26 +0200

    Update post_build.sh

--- post_build.sh
+++ post_build.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 set -u
 set -e
 
@@ -7,4 +5,3 @@ cp auto_wifi.psk "${TARGET_DIR}/var/lib/iwd/auto_wifi.psk"
 
 wget -O dejavu.zip https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_2_37/dejavu-fonts-ttf-2.37.zip
 unzip -p dejavu.zip dejavu-sans-ttf-2.37/ttf/DejaVuSans.ttf > "${TARGET_DIR}/usr/share/netsurf/DejaVuSans.ttf"
-
