8eb1d2e1418a58e5f33f04b330981af85cfcf5dc
le-krogoth <krogoth@mzd.org.uk>
2024-08-10 15:31:28 +0200

    removed netsurf tests

--- post_build.sh
+++ post_build.sh
@@ -7,9 +7,9 @@ set -e
 cp auto_wifi.psk "${TARGET_DIR}/var/lib/iwd/auto_wifi.psk"
 
 # make symlinks or netsurf wont find the fonts
-cd ${TARGET_DIR}/usr/share/netsurf/
+# cd ${TARGET_DIR}/usr/share/netsurf/
 # ln -s ${TARGET_DIR}/usr/share/fonts/dejavu/DejaVuSans.ttf DejaVuSans.ttf
-ln -s ${TARGET_DIR}/usr/share/fonts/dejavu/*.ttf .
+# ln -s ${TARGET_DIR}/usr/share/fonts/dejavu/*.ttf .
 
 # wget -O dejavu.zip https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_2_37/dejavu-fonts-ttf-2.37.zip
 # unzip -p dejavu.zip dejavu-sans-ttf-2.37/ttf/DejaVuSans.ttf > "${TARGET_DIR}/usr/share/netsurf/DejaVuSans.ttf"
